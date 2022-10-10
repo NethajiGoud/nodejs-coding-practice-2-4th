e25352442d983abaaa09819dff78ce63fdd86c44
NethajiGoud <114430294+NethajiGoud@users.noreply.github.com>
2022-10-11 00:12:08 +0530

    Update index.js

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -2,8 +2,8 @@ let ratioOfTwoNumbers = require("../ratio/index.js");
 let factorial = require("../factorial/index.js");
 
 module.exports = function ratioAndFactorial(num1, num2, num3) {
-  let two_numbers = ratioOfTwoNumbers(num1, num2);
-  let one_number = factorialOfNumber(num3);
+  let ratio = ratioOfTwoNumbers(num1, num2);
+  let factorial = factorialOfNumber(num3);
   let c = {
     ratio: two_numbers,
     factorial: one_number,
