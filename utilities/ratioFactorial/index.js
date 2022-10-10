f146de649c36f055c91ce6c99262d95336931171
NethajiGoud <114430294+NethajiGoud@users.noreply.github.com>
2022-10-11 00:12:39 +0530

    Update index.js

--- utilities/ratioFactorial/index.js
+++ utilities/ratioFactorial/index.js
@@ -5,8 +5,8 @@ module.exports = function ratioAndFactorial(num1, num2, num3) {
   let ratio = ratioOfTwoNumbers(num1, num2);
   let factorial = factorialOfNumber(num3);
   let c = {
-    ratio: two_numbers,
-    factorial: one_number,
+    ratio: ratio,
+    factorial: factorial,
   };
   return c;
 };
